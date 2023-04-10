e1f4962620a9d5f25efb451d3f59dc705b67e044
Verissimo <terceiroverissimo@gmail.com>
2023-04-10 17:39:47 -0300

    resolução do documento anexo e práticas livro CBB

---
+++ CBBpratica5.java
@@ -0,0 +1,8 @@
+public class CBBpratica5 {
+    public static void main(String[] args){
+        for(int i=-1; i < 100; i++){
+            if((i+1)%2 == 0)
+                System.out.println(i+1);
+        }
+    }
+}
